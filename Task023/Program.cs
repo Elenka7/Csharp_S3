d1e032198f653eeb70dcbbef5ebf3dd01b533f79
силивончик елена <e.silivonchik@sckkgit initgit config --global user.email e.silivonchik@sckk>
2023-03-31 19:45:21 +0300

    Решение задачи 23

---
+++ Task023/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите число больше 1: ");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num >= 1)
+{
+    for (double cube = 1; cube <= num; cube++)
+    {
+        Console.Write(Math.Pow(cube, 3) + ", ");
+    }
+}
+else
+{
+    Console.Write("Введено неверное число.");
+}
